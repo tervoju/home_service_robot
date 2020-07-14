2a430b6646237dfd2e60cd78389df467ea550242
Juha Tervo <jutervo@outlook.com>
2020-07-14 14:58:49 +0000

    script changes

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,5 +1,5 @@
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/home_service_robot/src/world/juhas.world " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/home_service_robot/src/world/juha2.world " &
 sleep 5
 
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/home_service_robot/src/map/map.yaml" &
